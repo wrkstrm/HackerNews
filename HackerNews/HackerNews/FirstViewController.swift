3f8fdcba014b703ee47786bbce498ba321b1924f
rismay <cmonterroza@wrkstrm.me>
2017-12-28 00:08:02 -0600

    New project.

---
+++ HackerNews/HackerNews/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  HackerNews
+//
+//  Created by rismay on 12/27/17.
+//  Copyright © 2017 wrkstrm. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
---
+++ HackerNews/HackerNews/SecondViewController.swift
@@ -0,0 +1,22 @@
+//
+//  SecondViewController.swift
+//  HackerNews
+//
+//  Created by rismay on 12/27/17.
+//  Copyright © 2017 wrkstrm. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
