7fecbf22a76e838c72867e8ec6bd2cc6060f25ea
rismay <cmonterroza@wrkstrm.me>
2017-12-29 14:55:28 -0600

    Add SwiftLint and remove warnings.

--- HackerNews/HackerNews/AppDelegate.swift
+++ HackerNews/HackerNews/AppDelegate.swift
@@ -13,34 +13,48 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+    /// Override point for customization after application launch.
+    ///
+    /// - Parameters:
+    ///   - application: The current application instance.
+    ///   - launchOptions: The app launch options.
+    /// - Returns: Wheter or not the app successfully boot.
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         return true
     }
 
+    /// Sent when the application is about to move from active to inactive state.
+    ///
+    /// - Parameter application: The current application instance.
     func applicationWillResignActive(_ application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
     }
 
+    /// If your application supports background execution,
+    /// this method is called instead of applicationWillTerminate: when the user quits.
+    ///
+    /// - Parameter application: The current application instance.
     func applicationDidEnterBackground(_ application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
     }
 
+    /// Called as part of the transition from the background to the active state;
+    /// here you can undo many of the changes made on entering the background.
+    ///
+    /// - Parameter application: The current application instance.
     func applicationWillEnterForeground(_ application: UIApplication) {
-        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
     }
 
+    /// Restart any tasks that were paused (or not yet started) while the application was inactive.
+    /// If the application was previously in the background, optionally refresh the user interface.
+    ///
+    /// - Parameter application: The current application instance.
     func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
     }
 
+    /// Called when the application is about to terminate.
+    /// Save data if appropriate. See also applicationDidEnterBackground:.
+    ///
+    /// - Parameter application: The current application instance.
     func applicationWillTerminate(_ application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
-
-
 }
-
