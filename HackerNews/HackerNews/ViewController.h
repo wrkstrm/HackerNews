b9af3122fcb7a98c36ae0ba46024767241d41d08
rismay <cmonterroza@wrkstrm.me>
2018-01-04 02:25:26 -0600

    Add bridging header and remove unnecessary library references.

---
+++ HackerNews/HackerNews/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  HackerNews
+//
+//  Created by rismay on 1/4/18.
+//  Copyright © 2018 wrkstrm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
